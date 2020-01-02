2d05515da72621de4d8f8aa68f0eff582bcde980
sexfat <bryant6831@gmail.com>
2020-01-02 12:11:11 +0800

    上午的課

---
+++ src/index.js
@@ -0,0 +1,10 @@
+console.log('ok');
+import "./style.css";
+import TweenMax from 'gsap';
+
+
+
+TweenMax.to('#logo' , 1 , {
+    x: 100,
+    repeat : -1
+})
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
+
+module.exports = {
+    entry: './src/index.js', // 入口文件
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.bundle.js'
+    }, // 出口文件
+    module: {
+        rules: [{
+          test: /\.css$/,
+          use: [{
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                // you can specify a publicPath here
+                // by default it use publicPath in webpackOptions.output
+                publicPath: './dist'
+              }
+            },
+            'css-loader'
+          ]
+        }]
+      },                // 處理對應模組
+    plugins: [
+        //html 5 plugin
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: "./style.css"
+        }),
+    ],             // 對應的插件
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        port: 3006,
+        index :"index.html",
+        open : true
+        
+
+    }, // 開發伺服器的
+    mode: 'development' //"production" | "development" | "none"  開發模式
+}
